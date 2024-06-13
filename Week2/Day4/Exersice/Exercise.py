6d2e08c9d1cb51d798bf4cc2686037a897f2e375
yonah-trachtman <yonah.trachtman@gmail.com>
2024-06-13 13:25:15 +0300

    Day4 exercise

---
+++ Week2/Day4/Exersice/Exercise.py
@@ -0,0 +1,45 @@
+# Exercise 1 : What are you learning ?
+# Instructions
+
+#     Write a function called display_message() that prints one sentence telling everyone what you are learning in this course.
+#     Call the function, and make sure the message displays correctly.
+
+# Code starts here!!!!
+def display_message():
+    return "I'm currently learning python"
+
+
+print(display_message())
+
+
+
+
+
+# 🌟 Exercise 2: What’s your favorite book ?
+# Instructions
+
+#     Write a function called favorite_book() that accepts one parameter called title.
+#     The function should print a message, such as "One of my favorite books is <title>".
+#     For example: “One of my favorite books is Alice in Wonderland”
+#     Call the function, make sure to include a book title as an argument when calling the function.
+
+
+# Code starts here!!!!
+def favorite_book(title):
+    print(f"One of my favorite books is {title}")
+
+
+
+favorite_book("Feet of Clay")
+
+
+
+
+# 🌟 Exercise 3 : Some Geography
+# Instructions
+
+#     Write a function called describe_city() that accepts the name of a city and its country as parameters.
+#     The function should print a simple sentence, such as "<city> is in <country>".
+#     For example “Reykjavik is in Iceland”
+#     Give the country parameter a default value.
+#     Call your function.
