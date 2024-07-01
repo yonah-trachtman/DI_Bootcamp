9313b4f403032d36352c1e00d43fe4022bc1f6fd
yonah-trachtman <yonah.trachtman@gmail.com>
2024-07-01 08:30:12 +0300

    commit

--- Week4/Day4/DC.py
+++ Week4/Day4/DC.py
@@ -21,7 +21,7 @@ print(response.status_code)
 to_json = response.json()
 
 
-cursor.execute("CREATE TABLE Countries (country_id serial primary key, name varchar(255), capital Varchar(255), flag varchar(max), subregion Varchar(255), population float)")
+cursor.execute("CREATE TABLE Countries (country_id serial primary key, name varchar(255), capital Varchar(255), flag varchar(255), subregion Varchar(255), population float)")
 def add_item(name, capital, flag, subregion, population):
     query = f'''INSERT INTO Countries (name, capital, flag, subregion, population)
 VALUES ('{name}', '{capital}', '{flag}', '{subregion}', {population}); '''
